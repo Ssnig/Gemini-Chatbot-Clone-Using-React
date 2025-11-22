9a9df8af078e2ab12e9004bfeca56470eb27a8b1
Ssnig <kyawsanh402@gmail.com>
2025-11-22 22:29:35 +0630

    AI integrated

---
+++ src/context/Context.jsx
@@ -0,0 +1,18 @@
+import { createContext } from 'react'
+import runChat from '../config/Gemini'
+export const Context = createContext();
+const ContextProvider =(props)=>{
+    const onSent = async (prompt) =>{
+        await runChat(prompt)
+    }
+    onSent("What Is React JS");
+
+    const contextValue ={
+    };
+    return(
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+export default ContextProvider;
